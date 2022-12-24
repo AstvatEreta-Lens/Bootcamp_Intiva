0f9f6b293a42d9d91351100ef56a22ef55f9662e
Ahmad Al <ashabuljannah201@gmail.com>
2022-12-24 09:26:44 +0700

    task1

---
+++ daily-task1.php
@@ -0,0 +1,23 @@
+<!-- 
+    Ahmad Al Wabil _ 105221020
+    1. Tipe data Program merupakan kalsfikasi data yang hanya diterima berdasarkan nilai tersebut agar komputer dapat mengenali data tersebut berdasarkan karakteristiknya
+    2. Boolean(logical binary true or false/ 1 or 0)
+       Integer(bilangan bulat)
+       floar(bilangan real)
+       string(sekumpulan berbagai jenis karakter yang harus diawali dengan alfabet)
+       char(menerima satu karakter)
+    3. aritmatika merupakan operasi dasar matematika yang memungkinkan melakukan operis penjumlahan, pengurangan, perngalian, pembagian, modulus, dll.
+ -->
+<?php
+$i = 10;
+$j = 11;
+
+$total = $i+$j;
+echo $total."</br>";
+$total = $i*$j;
+echo $total."</br>";
+$total = $i/$j;
+echo $total."</br>";
+$total = $i-$j;
+echo $total;
+?>
